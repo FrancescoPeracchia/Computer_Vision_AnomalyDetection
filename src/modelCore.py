8eb93450e5aabc3ab3cc55c6cb66197ffb295810
GiorgiaNatalizia <62596629+GiorgiaNatalizia@users.noreply.github.com>
2021-07-16 12:00:10 +0200

    Update modelCore.py

--- src/modelCore.py
+++ src/modelCore.py
@@ -19,6 +19,7 @@ from keras.legacy import interfaces
 from keras.engine import InputSpec
 import tensorflow as tf
 import numpy as np 
+import hdf5
 
 #################################################################################
 # Model Utils for Image Manipulation Classification
@@ -449,3 +450,83 @@ def load_pretrain_model_by_index( pretrain_index, model_dir ) :
     assert os.path.isfile(weight_file), "ERROR: fail to locate the pretrained weight file"
     single_gpu_model.load_weights( weight_file )
     return single_gpu_model
+
+def train_model(csv_file,ep,lr):
+    
+    #preparazione delle robe 
+    def Read_image(path):
+      image = Image.open(path).convert('RGB')
+      return image
+
+    X = []
+    Y = []
+    
+    dataset = pd.read_csv(csv_file)
+    for index, row in dataset.iterrows():
+        X.append(array(Read_image(row[0]).resize((100, 100))).flatten() / 255.0)
+        Y.append(row[1])
+
+    X = np.array(X)
+    Y = to_categorical(Y, 2)
+    X = X.reshape(-1, 100, 100, 3)
+
+    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.20, random_state=5)
+    
+    #parametri
+    epochs=ep
+    batch_size= 20
+    
+    #fit del modello
+    history = model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_val, Y_val),verbose=2)
+    #plottino
+    fig, ax = plt.subplots(3, 1)
+    ax[0].plot(history.history['loss'], color='b', label="Training loss")
+    ax[0].plot(history.history['val_loss'], color='r', label="validation loss", axes=ax[0])
+    legend = ax[0].legend(loc='best', shadow=True)
+
+    ax[1].plot(history.history['acc'], color='b', label="Training accuracy")
+    ax[1].plot(history.history['val_acc'], color='r', label="Validation accuracy")
+    legend = ax[1].legend(loc='best', shadow=True)
+    
+    #confusion matrix
+    def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, cm[i, j], horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        
+     # Predict the values from the validation dataset
+    Y_pred = model.predict(X_val)
+    # Convert predictions classes to one hot vectors
+    Y_pred_classes = np.argmax(Y_pred, axis=1)
+    # Convert validation observations to one hot vectors
+    Y_true = np.argmax(Y_val, axis=1)
+    # compute the confusion matrix
+    confusion_mtx = confusion_matrix(Y_true, Y_pred_classes)
+    # plot the confusion matrix
+    plot_confusion_matrix(confusion_mtx, classes=range(2))
+    
+    #robe che servono e path
+    file_number = get_a_random_pair #(sistemare era un random tra tot numeri)
+    image_path = os.getcwd()+"\\Figures" #path del dataset
+    Models_path = os.getcwd()+"\\Re_Traind_Models" #qui va messa la funzione del modello 
+    
+    plot_Name = image_path+"\\VGG16_"+str(file_number)+".png"
+    plt.savefig(plot_Name , transparent =True , bbox_incehs="tight" , pad_inches = 2 , dpi = 50)#forse non serve
+    #salviamo il modello
+    model.save(Model_Name)
+    model.save_weights('visiope_bestmodel.hdf5')
+    return plot_Name , Model_Name
